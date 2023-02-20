cb75cadd019f217f308e55b6f39b71e3b4f79b73
jplight <janpfister@t-online.de>
2023-02-20 12:59:57 +0100

    add top.gg link to cronmsg

--- bot/bot.py
+++ bot/bot.py
@@ -68,7 +68,9 @@ async def milestones(ctx: di.CommandContext):
 async def create_vote_message():
     text = f"Hey! Du kannst voten! {Emojis.vote_yes}\n" \
         f"Wenn du aktiv für den Server stimmst, bekommst und behältst du die <@&939557486501969951> Rolle!\n\n" \
-        f"**Voten:** https://discords.com/servers/moonfamily\n\nVielen Dank und viel Spaß! {Emojis.sleepy} {Emojis.crone} {Emojis.anime}"
+        f"**Voten:** https://discords.com/servers/moonfamily\n\nVielen Dank und viel Spaß! {Emojis.sleepy} {Emojis.crone} {Emojis.anime}" \
+        f"\n\n<@&1075849079638196395> Rolle für höhere Gewinnchancen bei Giveaways:\n" \
+        f"**Voten:** https://top.gg/de/servers/903713782650527744/vote"
     url = "https://cdn.discordapp.com/attachments/1009413427485216798/1070076437882736690/Moon_Family_Upvote_Button.png"
     embed = di.Embed(
         title=f"Voten und Unterstützer werden {Emojis.minecraft}",
