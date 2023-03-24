d2df4ee68e96d058d7b3c4dd630978a3d1f3b019
jplight <janpfister@t-online.de>
2023-03-24 14:19:06 +0100

    fix remove job from schedule
    Jobs were not removed from schedule when deleting the job

--- bot/ext/schedules.py
+++ bot/ext/schedules.py
@@ -58,7 +58,11 @@ class Schedule(di.Extension):
 
     def _del_schedule(self, id:int):
         self._sql_del_schedule(id=id)
-        if self._sched_activ.get(id):
+        job_data = self._sched_activ.get(id)
+        if job_data:
+            job_id = job_data.get("job_id", None)
+            if job_id:
+                self._schedule.remove_job(job_id=job_id)
             self._sched_activ.__delitem__(id)
 
 
