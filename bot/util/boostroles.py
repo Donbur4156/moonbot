86f58ec80a4eaecefa02f93d18e61d3a93d09f6f
jplight <janpfister@t-online.de>
2023-04-15 18:55:19 +0200

    fix booscolor response

--- bot/util/boostroles.py
+++ bot/util/boostroles.py
@@ -57,7 +57,7 @@ class BoostRoles:
         return role
 
     def get_embed_color(self, role: di.Role):
-        return di.Embed(description=f"Du hast dich für `{role.icon} {role.name}` entschieden und die neue Farbe im Chat erhalten.", color=0x43FA00)
+        return di.Embed(description=f"Du hast dich für `{role.name}` entschieden und die neue Farbe im Chat erhalten.", color=0x43FA00)
     
     def get_embed_icons(self, role: di.Role):
         return di.Embed(description=f"Du hast dich für `{role.icon}` entschieden und das neue Icon im Chat erhalten.", color=0x43FA00)
