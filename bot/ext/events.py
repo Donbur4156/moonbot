114f58a27586cfa0c7448d5617d27c53ef4ba622
jplight <janpfister@t-online.de>
2023-08-08 18:48:58 +0200

    add role after end of pending

--- bot/ext/events.py
+++ bot/ext/events.py
@@ -1,14 +1,17 @@
 import logging
 import random
+from types import NoneType
 
 import config as c
 import interactions as di
 from configs import Configs
+from deepdiff import DeepDiff
 from ext.welcomemsgs import read_txt
 from interactions import OrTrigger, Task, TimeTrigger, listen
-from interactions.api.events import MemberAdd, MemberRemove
+from interactions.api.events import MemberAdd, MemberRemove, MemberUpdate
 from util.emojis import Emojis
 from util.objects import DcUser
+from util.sql import SQL
 from whistle import EventDispatcher
 
 
@@ -19,6 +22,8 @@ class EventClass(di.Extension):
         self._logger: logging.Logger = kwargs.get("logger")
         self._dispatcher: EventDispatcher = kwargs.get("dispatcher")
         self.joined_member: dict[int, DcUser] = {}
+        self.new_members: set[int] = {}
+        self.sql = SQL(database=c.database)
 
     @listen()
     async def on_startup(self):
@@ -26,6 +31,7 @@ class EventClass(di.Extension):
         self.create_vote_message.start()
         self.set_wlc_msgs()
         self._dispatcher.add_listener("wlcmsgs_update", self.set_wlc_msgs)
+        self.get_new_members()
 
     def set_wlc_msgs(self, event=None):
         self.wlc_msgs = read_txt()
@@ -38,6 +44,35 @@ class EventClass(di.Extension):
             f"{Emojis.welcome} {Emojis.dance} {Emojis.crone}"
         )
 
+    def get_new_members(self):
+        self.new_members = {
+            member[0] 
+            for member 
+            in self.sql.execute(stmt = "SELECT user_id FROM new_members").data_all
+        }
+
+    def add_new_member(self, member_id: int):
+        self.sql.execute(stmt="INSERT INTO new_members(user_id) VALUES (?)", var=(member_id,))
+        self.new_members.add(member_id)
+
+    def del_new_member(self, member_id: int):
+        self.sql.execute(stmt="DELETE FROM new_members WHERE user_id=?", var=(member_id,))
+        self.new_members.discard(member_id)
+
+
+    @listen()
+    async def on_guild_member_update(self, event: MemberUpdate):
+        diffs = DeepDiff(event.before, event.after, exclude_paths="root.top_role", ignore_type_in_groups=[str, int, NoneType])
+        self._logger.debug(f"EVENT/MEMBERUPDATE/{event.after.id}; Diff: {diffs}")
+        if (int(event.after.id) not in self.new_members
+            and event.before.pending 
+            and not event.after.pending):
+            member = event.after
+            self.del_new_member(int(member.id))
+            await member.add_roles(roles=[903715839545598022, 905466661237301268, 913534417123815455])
+            self._logger.info(f"EVENT/Member end pending/{member.username} ({member.id})")
+
+
     @listen()
     async def on_guild_member_add(self, event: MemberAdd):
         if int(event.guild.id) != c.serverid: return False
@@ -47,8 +82,7 @@ class EventClass(di.Extension):
         channel = await self._config.get_channel("chat")
         dcuser.wlc_msg = await channel.send(self.gen_wlc_msg(member.mention))
         self.joined_member.update({int(member.id): dcuser})
-        await member.add_roles(roles=[903715839545598022, 905466661237301268, 913534417123815455])
-        # TODO: add_role Zeitversetzt evtl. erst wenn nicht mehr pending
+        self.add_new_member(int(member.id))
 
     @listen()
     async def on_guild_member_remove(self, event: MemberRemove):
