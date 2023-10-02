e8466db70120a42d9bfa2725e437e856ca5a7233
jplight <janpfister@t-online.de>
2023-10-02 21:32:08 +0200

    impl membercheck cron
    add respond for add default roles

--- bot/ext/events.py
+++ bot/ext/events.py
@@ -5,8 +5,8 @@ import config as c
 import interactions as di
 from configs import Configs
 from ext.welcomemsgs import read_txt
-from interactions import (ContextMenuContext, OrTrigger, Task, TimeTrigger,
-                          listen, user_context_menu)
+from interactions import (ContextMenuContext, IntervalTrigger, OrTrigger, Task,
+                          TimeTrigger, listen, user_context_menu)
 from interactions.api.events import MemberAdd, MemberRemove, MemberUpdate
 from util.emojis import Emojis
 from util.objects import DcUser
@@ -31,6 +31,7 @@ class EventClass(di.Extension):
         self.set_wlc_msgs()
         self._dispatcher.add_listener("wlcmsgs_update", self.set_wlc_msgs)
         self.get_new_members()
+        self.check_new_members.start()
 
     def set_wlc_msgs(self, event=None):
         self.wlc_msgs = read_txt()
@@ -60,8 +61,11 @@ class EventClass(di.Extension):
 
     @user_context_menu(name="add default roles", dm_permission=False, default_member_permissions=di.Permissions.MODERATE_MEMBERS)
     async def add_default_roles_ctx(self, ctx: ContextMenuContext):
-        await self.add_default_roles(ctx.target) 
-        self._logger.info(f"USERCTX/add default roles/{ctx.target.username} ({ctx.target.id}) Teammember: {ctx.member.id}")
+        member = ctx.target
+        await self.add_default_roles(member) 
+        self.del_new_member(int(member.id))
+        self._logger.info(f"USERCTX/add default roles/{member.username} ({member.id}) Teammember: {ctx.member.id}")
+        await ctx.send(content=f"Dem User {member.mention} wurden die Default Rollen zugewiesen.", ephemeral=True)
 
     async def add_default_roles(self, member: di.Member):
         await member.add_roles(roles=[903715839545598022, 905466661237301268, 913534417123815455, 1143226806732853371])
@@ -100,6 +104,16 @@ class EventClass(di.Extension):
         self.del_new_member(int(member.id))
         if dcuser:
             await dcuser.delete_wlc_msg()
+
+    @Task.create(IntervalTrigger(minutes=2))
+    async def check_new_members(self):
+        for member_id in self.new_members:
+            member = await self._client.fetch_member(user_id=member_id, guild_id=c.serverid)
+            if not member.pending:
+                await self.add_default_roles(member) 
+                self.del_new_member(member_id)
+                self._logger.info(f"CRON/add default roles/{member.username} ({member.id})")
+                break
 
     @Task.create(OrTrigger(
             TimeTrigger(hour=0, utc=False),
