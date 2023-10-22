765a89651e0aad1727cb1144669dddbc8fab4d16
jplight <janpfister@t-online.de>
2023-10-22 17:08:28 +0200

    add Guildcheck

--- bot/ext/dev.py
+++ bot/ext/dev.py
@@ -3,8 +3,9 @@ import tempfile
 import time
 
 import interactions as di
+import config as c
 from configs import Configs
-from interactions import SlashCommand, slash_command, slash_option
+from interactions import SlashCommand, slash_command, slash_option, listen
 from whistle import EventDispatcher
 
 
@@ -14,6 +15,22 @@ class DevClass(di.Extension):
         self._config: Configs = kwargs.get("config")
         self._logger: logging.Logger = kwargs.get("logger")
         self._dispatcher: EventDispatcher = kwargs.get("dispatcher")
+
+    @listen()
+    async def on_startup(self):
+        for guild in self._client.guilds:
+            if int(guild.id) not in c.server_whitelist:
+                donbur = await self._client.fetch_user(c.donbur)
+                await donbur.send(f"Moon Bot hat den Server {guild.name} ({guild.id}) verlassen.\nDieser ist nicht auf der Whitelist.")
+                await guild.leave()
+
+    @listen("GuildJoin")
+    async def onguildjoin(self, event: di.events.GuildJoin):
+        if not self._client.is_ready: return
+        guild = event.guild
+        donbur = await self._client.fetch_user(c.donbur)
+        await donbur.send(f"Der Moon Bot ist dem Server **{guild.name}** ({guild.id}) beigetreten.")
+            
 
     devCmds = SlashCommand(name="dev", description="developer Commands", scopes=[1009456838615507005])
 
