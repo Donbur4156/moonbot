365ff9e81f37c8af8cae4b63d9065c1e44cb36ef
jplight <janpfister@t-online.de>
2024-04-21 16:44:49 +0200

    fix imports

--- bot/ext/drop_list/__init__.py
+++ bot/ext/drop_list/__init__.py
@@ -1,5 +1,5 @@
-from ext.drop_list.boostcolor import BoostColResponse, Drop_BoostColor
-from ext.drop_list.drop import Drop
-from ext.drop_list.emoji import Drop_Emoji, EmojiResponse
-from ext.drop_list.starpowder import Drop_StarPowder
-from ext.drop_list.vip import Drop_VIP_Rank
+from .drop import Drop
+from .boostcolor import BoostColResponse, Drop_BoostColor
+from .emoji import Drop_Emoji, EmojiResponse
+from .starpowder import Drop_StarPowder
+from .vip import Drop_VIP_Rank
